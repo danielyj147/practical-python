e828585d3ab55e8f16702b4a0cca28769a100652
Daniel Jeong <danielyj147@gmail.com>
2023-07-20 17:34:57 +0900

    python lesson

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+def bounce(height, n):
+  starting_height = height
+  for i in range(n):
+    starting_height = starting_height * 0.6
+    print(i, round(starting_height, 4))
+    
+bounce(100, 10)
